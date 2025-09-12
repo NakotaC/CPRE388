32fe3d2665aef605c072ebf6c82bcc03762bf0d0
NakotaC <Nakotaclark@gmail.com>
2025-09-12 15:32:22 -0500

    Create basic files for Lab3

---
+++ Stopwatch/app/src/main/java/com/example/stopwatch/StopwatchViewModel.java
@@ -0,0 +1,13 @@
+package com.example.stopwatch;
+
+public class StopwatchViewModel extends ViewModel {
+
+    // Variable to track elapsed time
+    private long elapsedTime = 0L;
+
+    // TODO: Add methods to start, stop, and reset the stopwatch
+
+    public long getElapsedTime() {
+        return elapsedTime;
+    }
+}
