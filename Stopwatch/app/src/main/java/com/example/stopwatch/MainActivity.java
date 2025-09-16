10418ffa933ac905533266af337a65ed84407e25
NakotaC <Nakotaclark@gmail.com>
2025-09-16 15:22:31 -0500

    Made some progress on Stopwatch. Figuring out LiveData and ViewModels

--- Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.java
+++ Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.stopwatch;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
 
--- Stopwatch/app/src/main/java/com/example/stopwatch/StopwatchViewModel.java
+++ Stopwatch/app/src/main/java/com/example/stopwatch/StopwatchViewModel.java
@@ -1,13 +1,24 @@
 package com.example.stopwatch;
 
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
 public class StopwatchViewModel extends ViewModel {
 
     // Variable to track elapsed time
-    private long elapsedTime = 0L;
+    private MutableLiveData<Long> elapsedTime = new MutableLiveData<>(0L);;
 
     // TODO: Add methods to start, stop, and reset the stopwatch
+    public void start() {
 
+    }
+    public void stop() {
+
+    }
+    public void reset() {
+
+    }
     public long getElapsedTime() {
-        return elapsedTime;
+        return elapsedTime.getValue();
     }
 }
